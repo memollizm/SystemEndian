4e41f4e149319e986c72d84079ec097093b4d793
Mehmet Başgöze <92752077+memollizm@users.noreply.github.com>
2022-11-30 17:44:17 +0300

    Add files via upload

---
+++ System.BitConverter1/System.BitConverter1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.BitConverter1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                Console.WriteLine("Little Endian");
+            }
+            else
+            {
+                Console.WriteLine("Big Endian");
+            }
+            int a = 46513;
+            byte[] b = BitConverter.GetBytes(a);
+
+            foreach(byte x in b)
+                Console.WriteLine(x);
+
+            Console.ReadKey();
+        }
+    }
+}
